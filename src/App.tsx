02d606752edb3ccfc9696d2ff1aa04bf5feb7b4c
Mincheol Kim <mincheol@thingsw.com>
2021-01-15 17:40:51 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import update from "immutability-helper";
+
+import { Box } from "./Box";
+import { useDrop, XYCoord } from "react-dnd";
+
+export interface DragItem {
+  type: string;
+  id: string;
+  top: number;
+  left: number;
+}
 
 function App() {
+  const [boxes, setBoxes] = useState<{
+    [key: string]: {
+      top: number;
+      left: number;
+      file: string;
+    };
+  }>({});
+
+  const [zoom, setZoom] = useState(1);
+
+  const [, drop] = useDrop({
+    accept: "image",
+    drop(item: DragItem, monitor) {
+      const delta = monitor.getDifferenceFromInitialOffset() as XYCoord;
+      const left = Math.round(item.left + delta.x);
+      const top = Math.round(item.top + delta.y);
+      moveBox(item.id, left, top);
+      return undefined;
+    },
+  });
+
+  const moveBox = (id: string, left: number, top: number) => {
+    setBoxes(
+      update(boxes, {
+        [id]: {
+          $merge: { left, top },
+        },
+      })
+    );
+  };
+
+  const openFile1 = (evt: ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.files) {
+      setBoxes(
+        update(boxes, {
+          $merge: {
+            0: {
+              left: 0,
+              top: 0,
+              file: URL.createObjectURL(evt.target.files[0]),
+            },
+          },
+        })
+      );
+    }
+  };
+  const openFile2 = (evt: ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.files) {
+      setBoxes(
+        update(boxes, {
+          $merge: {
+            1: {
+              left: 0,
+              top: 0,
+              file: URL.createObjectURL(evt.target.files[0]),
+            },
+          },
+        })
+      );
+    }
+  };
+
+  console.log("zoom", zoom);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ zIndex: 99 }}>
+        <input type="file" onChange={openFile1} />
+        <input type="file" onChange={openFile2} />
+        <button onClick={() => setZoom((z) => z + 0.2)}>zoom in</button>
+        <button onClick={() => setZoom(1)}>set original</button>
+        <button onClick={() => setZoom((z) => z - 0.2)}>zoom out</button>
+      </div>
+      <div
+        ref={drop}
+        style={{
+          position: "relative",
+          width: "100vw",
+          height: "100vh",
+          transform: `scale(${zoom})`,
+          transformOrigin: "top left",
+        }}
+      >
+        {Object.keys(boxes).map((key) => {
+          const { left, top, file } = boxes[key];
+          return (
+            <Box key={key} id={key} left={left} top={top} hideSourceOnDrag>
+              <img src={file} style={{ opacity: 0.5 }} />
+            </Box>
+          );
+        })}
+      </div>
     </div>
   );
 }
